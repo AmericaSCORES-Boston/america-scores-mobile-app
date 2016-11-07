cc58932a0de9335f612930c6e3b21746ea620e23
Jesse Oberstein <oberstein.j@husky.neu.edu>
2016-11-06 23:59:49 -0500

    AS-75: Added initial tests for components, actions, and reducers for BMI Collection.

---
+++ __tests__/bmi-collection-tests.js
@@ -0,0 +1,156 @@
+/**
+ * ********************************************
+ *   PAGE FLOW
+ * ********************************************
+ * 1) User begins BMI Collection
+ * 2) Student detail view is shown (students with information should *probably* not be shown)
+ * 3) User decides:
+ *    a) User enters info about student (repeat from step 3)
+ *    b) Clicks 'Save' (repeat from step 2)
+ *    c) Clicks 'Stop'
+ *    d) Clicks 'Previous Student' (repeat from step 2) (optional)
+ *
+ * ********************************************
+ *   COMPONENTS
+ * ********************************************
+ * -- StudentDetailsContainer (manages state)
+ *    -- StudentDetail (contains stats/info)
+ *    -- 'Save' Button (save this student and show next one)
+ *    -- 'Stop' Button
+ *    -- 'Previous Student' Button (optional)
+ *
+ * ********************************************
+ *   ACTIONS
+ * ********************************************
+ *  -- Start Collection (pass in current roster, filter students who don't have a height or weight entered)
+ *  -- End Collection
+ *      
+ * -- Async:
+ *   -- Save Student:
+ *      -- Request to save student
+ *      -- Successful save of student
+ *      -- Failure to save student
+ *
+ * ********************************************
+ *   REDUCERS
+ * ********************************************
+ * -- BMICollection State (updates current student, and maintains list of students missing height or weight)
+ * -- StudentDetail State
+ *      -- Request to save student - show 'Saving data for [student].'
+ *      -- Success on saving student - show '[Student]'s data has been saved.' Update navigation
+ *      -- Failure to save student - show feedback and student with saved input
+ *
+*/
+
+'use strict';
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import BMICollectionStudentDetail from '../config/bmi-collection/BMICollectionStudentDetail.react';
+import NextStudentButton from '../config/bmi-collection/NextStudentButton.react';
+import StopCollectionButton from '../config/bmi-collection/StopCollectionButton.react'
+import * as MockStudents from '../config/bmi-collection/data/MockStudents.js';
+
+/**
+ * *************************************************
+ *   COMPONENT TESTS
+ * *************************************************
+ */
+test('it renders the student1 detail view correctly', () => {
+    const tree = renderer.create(
+        <BMICollectionStudentDetail student={MockStudents.student1}></BMICollectionStudentDetail>
+).toJSON();
+expect(tree).toMatchSnapshot();
+});
+
+test('it renders "Save" button correctly', () => {
+    const tree = renderer.create(
+        <NextStudentButton></NextStudentButton>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it renders "Stop" button correctly', () => {
+    const tree = renderer.create(
+        <StopCollectionButton></StopCollectionButton>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+
+/**
+ * *************************************************
+ *   ACTION CREATOR TESTS
+ * *************************************************
+ */
+import * as actions from '../config/bmi-collection/BMICollectionActions'
+
+it('creates a "START_BMI_COLLECTION" action', () => {
+    expect(actions.startBMICollection(MockStudents.students)).toMatchSnapshot();
+});
+
+it('creates an "END_BMI_COLLECTION" action', () => {
+    expect(actions.endBMICollection(MockStudents.students)).toMatchSnapshot();
+});
+
+// Async action tests may need to be reworked since they are more complicated,
+// but this is the basic idea.
+
+it('creates a "SAVE_STUDENT" action', () => {
+    expect(actions.saveStudent(MockStudents.student1)).toMatchSnapshot();
+});
+
+it('creates a "SAVE_STUDENT_SUCCESS" action', () => {
+    expect(actions.saveStudent(MockStudents.student1)).toMatchSnapshot();
+});
+
+it('creates a "SAVE_STUDENT_FAILURE" action', () => {
+    expect(actions.saveStudent(MockStudents.badStudent)).toMatchSnapshot();
+});
+
+
+/**
+ * *************************************************
+ *   REDUCER TESTS
+ * *************************************************
+ */
+import * as reducer from '../config/bmi-collection/BMICollectionReducer';
+
+it('returns the same state on an unhandled action', () => {
+    expect(reducer({type: '_NULL'})).toMatchSnapshot();
+});
+
+it('starts BMI collection with a filtered list', () => {
+    const action = actions.startBMICollection(MockStudents.students);
+    expect(reducer(action).toMatchSnapshot();
+});
+
+it('starts BMI collection', () => {
+    const action = actions.startBMICollection(MockStudents.studentsWithoutHeightAndWeight);
+    expect(reducer(action)).toMatchSnapshot();
+});
+
+it('does not start BMI collection because there are no students created for this program', () => {
+    const action = actions.startBMICollection([]);
+    expect(reducer(action)).toMatchSnapshot();
+});
+
+it('does not start BMI collection because height and weight data has already been collected for all students', () => {
+    const action = actions.startBMICollection(MockStudents.studentsWithHeightAndWeight);
+    expect(reducer(action)).toMatchSnapshot();
+});
+
+it('makes a request to save the data entered for a student', () => {
+    const action = actions.saveStudent(MockStudents.student1);
+    expect(reducer(MockStudents.studentsWithoutHeightAndWeight, action)).toMatchSnapshot();
+});
+
+it('saves the student data successfully', () => {
+    const action = actions.onSaveStudentFailure(MockStudents.student1);
+    expect(reducer(MockStudents.studentsWithoutHeightAndWeight, action)).toMatchSnapshot();
+});
+
+it('fails to save the student data', () => {
+    const action = actions.onSaveStudentFailure(MockStudents.badStudent);
+    expect(reducer(MockStudents.studentsWithoutHeightAndWeight, action)).toMatchSnapshot();
+});
