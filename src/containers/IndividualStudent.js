ede76f7fc50d97d9bced83dfb61eae2958c848d4
Cosinity <accostenoble@gmail.com>
2016-12-04 22:54:44 -0500

    All stats are displayed. Fixed style.

--- src/containers/IndividualStudent.js
+++ src/containers/IndividualStudent.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { View, Text, Alert } from 'react-native';
-import { Container, Content, InputGroup, Input, H2 } from 'native-base';
+import { Container, Content, InputGroup, Input, H2 , Picker } from 'native-base';
 import { connect } from 'react-redux';
 import * as actions from '../actions/individualStudent';
 
@@ -10,7 +10,6 @@ class IndividualStudentContainer extends Component {
   constructor(props) {
     super(props);
     this.state = props.individualStudentState;
-
 
     this.props.component.onRight = () => {
       if (this.saveStudent())
@@ -35,7 +34,7 @@ class IndividualStudentContainer extends Component {
   saveStudent() {
     try {
       const currentStudent = this.state.student[0];
-      const currentStats = this.state.stats[0];
+      const currentStats = this.state.stats[this.state.stats.length - 1];
       const newStudent = { student_id: this.props.stud_id, first_name: currentStudent.first_name,
         last_name: currentStudent.last_name, dob: currentStudent.dob };
       this.props.updateStudent(newStudent);
@@ -68,6 +67,7 @@ class IndividualStudentContainer extends Component {
 
   render() {
     if (this.state.student != null && this.state.stats != null) {
+      console.log(this.state.student[0]);
       const currentStudent = this.state.student[0];
       var currentStats = { height: 0, weight: 0, pacer: 0 };
       if (this.state.stats.length != 0) {
@@ -76,9 +76,6 @@ class IndividualStudentContainer extends Component {
       }
       const bib_number = this.props.bib_num;
 
-      console.log(currentStudent);
-      console.log(currentStats);
-
       return (
         <Container style={[styles.container, styles.grayBg]}>
           <Content>
@@ -86,84 +83,150 @@ class IndividualStudentContainer extends Component {
             <Text style={[styles.largerText, styles.textAlignCenter]}>{bib_number}</Text>
             <View style={styles.mediumMarginTop}>
               <H2>Name</H2>
-              <View style={{flex: 1, flexDirection: 'row'}}>
-                <InputGroup style={[styles.inputGroup, {flex: 1}]}>
-                  <Input style={{flex: 1}}
-                         ref="first_name"
-                         autoCorrect={false}
-                         defaultValue={currentStudent.first_name}
-                         onChangeText={(firstName) => this.state.student[0].first_name = firstName}
-                         returnKeyType="done"
-                  />
-                </InputGroup>
-                <Text style={{flex: .5, paddingTop: 9}}></Text>
-                <InputGroup style={[styles.inputGroup, {flex: 1}]}>
-                  <Input style={{flex: 1}}
-                         ref="last_name"
-                         autoCorrect={false}
-                         defaultValue={currentStudent.last_name}
-                         returnKeyType="done"
-                  />
-                </InputGroup>
-                <Text style={{flex: .5, paddingTop: 9}}></Text>
+              <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{flex: .75, flexDirection: 'row', paddingRight: 10 }}>
+                  <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                    <Input ref="first_name"
+                           autoCorrect={false}
+                           defaultValue={currentStudent.first_name}
+                           onChangeText={(firstName) => this.state.student[0].first_name = firstName}
+                           returnKeyType="done"
+                    />
+                  </InputGroup>
+                </View>
+
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                    <Input ref="last_name"
+                           autoCorrect={false}
+                           defaultValue={currentStudent.last_name}
+                           onChangeText={(lastName) => this.state.student[0].last_name = lastName}
+                           returnKeyType="done"
+                    />
+                  </InputGroup>
+                </View>
+              </View>
+            </View>
+            <View style={styles.mediumMarginTop}>
+              <H2>Date of Birth</H2>
+              <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{flex: .75, flexDirection: 'row', paddingRight: 10 }}>
+                  <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                    <Input style={{flex: 1}}
+                           ref="day"
+                           keyboardType="numeric"
+                           defaultValue={currentStudent.dob.substring(8,10)}
+                           onChangeText={(day) => this.state.student[0].dob =
+                            currentStudent.dob.substring(0,7) + day + currentStudent.dob.substring(10)}
+                           returnKeyType="done"
+                           maxLength={2}
+                    />
+                  </InputGroup>
+                </View>
+
+                <View style={{flex: .75, flexDirection: 'row'}}>
+                  <Picker
+                    style={{flex: 1}}
+                    iosHeader="Month"
+                    mode="dropdown"
+                    selectedValue={this.state.student[0].dob.substring(5,7)}
+                    onValueChange={(newMonth) => this.state.student[0].dob =
+                        currentStudent.dob.substring(0,5) + newMonth + currentStudent.dob.substring(7)} >
+
+                    <Picker.Item label="Month" value="" />
+                    <Picker.Item label="Jan."  value="01" />
+                    <Picker.Item label="Feb."  value="02" />
+                    <Picker.Item label="Mar."  value="03" />
+                    <Picker.Item label="Apr."  value="04" />
+                    <Picker.Item label="May"   value="05" />
+                    <Picker.Item label="Jun."  value="06" />
+                    <Picker.Item label="Jul."  value="07" />
+                    <Picker.Item label="Aug."  value="08" />
+                    <Picker.Item label="Sep."  value="09" />
+                    <Picker.Item label="Oct."  value="10" />
+                    <Picker.Item label="Nov."  value="11" />
+                    <Picker.Item label="Dec."  value="12" />
+                  </Picker>
+                </View>
+
+                <View style={{flex: .75, flexDirection: 'row'}}>
+                  <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                    <Input style={{flex: 1}}
+                           ref="year"
+                           keyboardType="numeric"
+                           defaultValue={currentStudent.dob.substring(0,4)}
+                           onChangeText={(year) => this.state.student[0].dob =
+                              year + currentStudent.dob.substring(4)}
+                           returnKeyType="done"
+                           maxLength={4}
+                    />
+                  </InputGroup>
+                </View>
               </View>
             </View>
             <View style={styles.mediumMarginTop}>
               <H2>Height</H2>
-              <View style={{flex: 1, flexDirection: 'row'}}>
-                <InputGroup style={[styles.inputGroup, {flex: 1}]}>
-                  <Input style={{flex: 1}}
-                         ref="feet"
-                         keyboardType="numeric"
-                         defaultValue={this.getHeight(currentStats).feet.toString()}
-                         returnKeyType="done"
-                         maxLength={1}
-                  />
-                </InputGroup>
-                <Text style={{flex: .5, paddingTop: 9}}>ft</Text>
-                <InputGroup style={[styles.inputGroup, {flex: 1, paddingTop: 3}]}>
-                  <Input style={{flex: 1}}
-                         ref="inches"
-                         keyboardType="numeric"
-                         defaultValue={this.getHeight(currentStats).inches.toString()}
-                         returnKeyType="done"
-                         maxLength={2}
-                  />
-                </InputGroup>
-                <Text style={{flex: .5, paddingTop: 9}}>in</Text>
+              <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{flex: .75, flexDirection: 'row', paddingRight: 10 }}>
+                  <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                    <Input ref="feet"
+                           keyboardType="numeric"
+                           defaultValue={this.getHeight(currentStats).feet.toString()}
+                           returnKeyType="done"
+                           maxLength={1}
+                    />
+                  </InputGroup>
+                  <Text style={{flex: .5, paddingTop: 9}}>ft</Text>
+                </View>
+
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                    <Input ref="inches"
+                           keyboardType="numeric"
+                           defaultValue={this.getHeight(currentStats).inches.toString()}
+                           returnKeyType="done"
+                           maxLength={2}
+                    />
+                  </InputGroup>
+                  <Text style={{flex: .5, paddingTop: 9}}>in</Text>
+                </View>
               </View>
             </View>
             <View style={styles.mediumMarginTop}>
               <H2>Weight</H2>
               <View style={{flex: 1, flexDirection: 'row'}}>
-                <InputGroup style={[styles.inputGroup, {flex: 1}]}>
-                  <Input style={{flex: 1}}
-                         ref="weight"
-                         keyboardType="numeric"
-                         defaultValue={currentStats.weight.toString()}
-                         returnKeyType="done"
-                         maxLength={3}
-                  />
-                </InputGroup>
-                <Text style={{flex: 2, paddingTop: 9}}>lbs</Text>
+                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                  <View style={{flex: .75, flexDirection: 'row', paddingRight: 10 }}>
+                    <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                      <Input ref="weight"
+                             keyboardType="numeric"
+                             defaultValue={currentStats.weight.toString()}
+                             returnKeyType="done"
+                             maxLength={3}
+                      />
+                    </InputGroup>
+                  </View>
+                  <Text style={{flex: 2, paddingTop: 9}}>lbs</Text>
+                </View>
               </View>
             </View>
             <View style={styles.mediumMarginTop}>
               <H2>PACER</H2>
-              <View style={{flex: 1, flexDirection: 'row'}}>
-                <InputGroup style={[styles.inputGroup, {flex: 1}]}>
-                  <Input style={{flex: 1}}
-                         ref="pacer"
-                         keyboardType="numeric"
-                         defaultValue={currentStats.pacer.toString()}
-                         returnKeyType="done"
-                         maxLength={3}
-                  />
-                </InputGroup>
-                <Text style={{flex: 2, paddingTop: 9}}>laps</Text>
+              <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{flex: .75, flexDirection: 'row', paddingRight: 10 }}>
+                  <InputGroup style={[styles.InputGroup, {flex: 1}]}>
+                    <Input ref="pacer"
+                           keyboardType="numeric"
+                           defaultValue={currentStats.pacer.toString()}
+                           returnKeyType="done"
+                           maxLength={3}
+                    />
+                  </InputGroup>
+                  <Text style={{flex: 2, paddingTop: 9}}>laps</Text>
+                </View>
               </View>
             </View>
-          </Content>
+        </Content>
         </Container>
       );
     }
