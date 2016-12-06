d1314de55222355ac99ee9ab0be697cf9e573418
dalelyunas <alelyunas.d@husky.neu.edu>
2016-12-05 21:35:23 -0500

    more pacer code (potential intervals, state for levels/shuttles, more)

--- src/containers/Pacer.js
+++ src/containers/Pacer.js
@@ -1,20 +1,25 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { ListView, View, TouchableOpacity, Text } from 'react-native';
-import { Container, Header, Content, Button, H1, H2, H3 } from 'native-base';
+import { Container, Content, Button, H1, H2 } from 'native-base';
 import scoresTheme from '../themes/scoresTheme';
 import { connect } from 'react-redux';
+import pacerStages from '../util/pacerStages';
 import * as actions from '../actions/pacer';
 
 import styles from '../styles';
+
+const Sound = require('react-native-sound');
 
 class PacerContainer extends Component {
   constructor(props) {
     super(props);
     this.state = props.pacerState;
     this.state.pacerArray = [];
-    this.state.level = 1;
-    this.state.shuttle = 2;
-    this.state.elapsed = '00';
+    this.state.currentLevel = 0;
+    this.state.currentShuttle = 0;
+    this.state.totalShuttles = 0;
+    this.state.disabled = false;
+    this.state.pacerAudio = null;
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state.dataSource = ds.cloneWithRows(this.state.pacerArray);
   }
@@ -27,6 +32,93 @@ class PacerContainer extends Component {
     const newPacerState = nextProps.pacerState;
     if (newPacerState && newPacerState.pacerArray) {
       this.state.dataSource = this.state.dataSource.cloneWithRows( newPacerState.pacerArray );
+      this.state.pacerArray = newPacerState.pacerArray;
+      this.state.totalShuttles = newPacerState.totalShuttles;
+      this.state.currentShuttle = newPacerState.currentShuttle;
+      this.state.currentLevel = newPacerState.currentLevel;
+    }
+  }
+
+  isPacerTestOver() {
+    // Check pacerArray for full completion (all = 2)
+    const array = this.state.pacerArray;
+    if (this.state.pacerArray.length == 0) {
+      return false;
+    }
+
+    for (const item of array) {
+      console.log(item);
+      if (item != 2) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  clearPacerTest() {
+    // Stop the audio
+    this.state.pacerAudio.stop();
+    this.state.pacerAudio.release();
+    clearTimeout(this.state.currentTimeout);
+  }
+
+  startPacerTest() {
+    // Start the audio
+    this.state.pacerAudio = new Sound('PacerAudio.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        // App doesn't work?
+      } else {
+        console.log('duration in seconds: ' + this.state.pacerAudio.getDuration());
+        this.state.disabled = true;
+        this.forceUpdate();
+        this.state.pacerAudio.play();
+        // The first timeout in timeStages has to be equal to the timeout value
+        // of the first pacerStage and the first pacerStage has its levels decremented
+        // by one to keep everything even
+
+        // Timeout to wait for the intro of the tape
+        //this.state.currentTimeout = setTimeout(() => {this.timeStages(8470)}, 37500);
+      }
+    });
+  }
+
+  timeStages(interval) {
+    this.state.pacerAudio.getCurrentTime((seconds) => console.log('at ' + seconds));
+    this.state.currentTimeout = setTimeout(() => {
+      const stage = pacerStages[this.state.currentLevel];
+      const nextStage = pacerStages[this.state.currentLevel + 1];
+      if (this.isPacerTestOver()) {
+        this.clearPacerTest();
+      }
+      else if (this.state.currentShuttle >= stage.laps) {
+        this.props.maxShuttlesReached();
+        if (this.state.currentLevel < 21) {
+          this.timeStages(nextStage.time);
+        } else {
+          this.clearPacerTest();
+        }
+      } else {
+        this.props.timeIntervalElapsed();
+        this.timeStages(stage.time);
+      }
+    }, interval);
+  }
+
+  incrementShuttle() {
+    const stage = pacerStages[this.state.currentLevel];
+    if (this.isPacerTestOver()) {
+      this.clearPacerTest();
+    }
+    else if (this.state.currentShuttle >= stage.laps) {
+      this.props.timeIntervalElapsed();
+      this.props.maxShuttlesReached();
+      if (this.state.currentLevel > 21) {
+        this.clearPacerTest();
+      }
+    }
+    else {
+      this.props.timeIntervalElapsed();
     }
   }
 
@@ -34,13 +126,16 @@ class PacerContainer extends Component {
     if (rowData < 2) {
       this.props.incrementSquare(rowId);
     }
+
     // Set the student's total shuttles here
+    this.props.students[parseInt(rowId, 10)].pacer = this.state.currentLevel;
     // Maybe modify the passed in students array with a new field?
   }
 
   handlePacerHold(rowData, rowId) {
     if (rowData > 0) {
       // Unset the student's total shuttles here if it was set
+      this.props.students[parseInt(rowId, 10)].pacer = null;
       this.props.decrementSquare(rowId);
     }
   }
@@ -71,21 +166,22 @@ class PacerContainer extends Component {
       <Container style={[styles.container, styles.containerPadding]}>
           <Content theme={scoresTheme}>
             <View style={[{flex: 1, flexDirection: 'row', justifyContent: 'center'}, styles.smallMarginTop]}>
-              <H2 style={{marginRight: 20}}>Level: {this.state.level}</H2>
-              <H2>Shuttle: {this.state.shuttle}</H2>
+              <H2 style={{marginRight: 20}}>Level: {this.state.currentLevel + 1}</H2>
+              <H2>Shuttle: {this.state.totalShuttles}</H2>
             </View>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-              <View style={[{borderColor: '#E4E4E4', borderWidth: 3, width: 130}, styles.mediumMarginTop]}>
-                <H2 style={[styles.textCenter, styles.smallMarginTop]}>Total Shuttles</H2>
-                <H1 style={[styles.textCenter, styles.smallMarginTop, styles.largeText]}>{this.state.elapsed}</H1>
-              </View>
-            </View>
+            <Button large block onPress={() => this.incrementShuttle()} style={styles.smallVerticalMargin}>
+                <H1 style={styles.white}>Next Shuttle</H1>
+            </Button>
             <ListView contentContainerStyle={[styles.gridList, styles.mediumMarginTop]}
               dataSource={this.state.dataSource}
               renderRow={(rowData, seciondId, rowId) => this.renderSquares(rowData, rowId)}
               enableEmptySections={true}
             />
+            <Button large block disabled = {this.state.disabled} onPress={() => this.startPacerTest()} style={styles.smallVerticalMargin}>
+              <H1 style={styles.white}>Start Test</H1>
+            </Button>
           </Content>
+
       </Container>
     );
   }
@@ -104,6 +200,12 @@ const mapDispatchToProps = (dispatch) => ({
   },
   decrementSquare: (index) => {
     dispatch(actions.pacerItemLongPress(index));
+  },
+  timeIntervalElapsed: () => {
+    dispatch(actions.timeIntervalElapsed());
+  },
+  maxShuttlesReached: () => {
+    dispatch(actions.maxShuttlesReached());
   }
 });
 
--- src/reducers/pacer.js
+++ src/reducers/pacer.js
@@ -1,14 +1,19 @@
 import {
   LOAD_PACER_TEST,
   PACER_ITEM_TAPPED,
-  PACER_ITEM_LONGPRESS
+  PACER_ITEM_LONGPRESS,
+  TIME_INTERVAL_ELAPSED,
+  MAX_SHUTTLES_REACHED
 } from '../actions/pacer';
 
-export default function bmiState(state = {}, action) {
+export default function pacerState(state = {}, action) {
   switch (action.type) {
     case LOAD_PACER_TEST:
       return {
         ...state,
+        currentLevel: 0,
+        currentShuttle: 0,
+        totalShuttles: 0,
         pacerArray: new Array(action.numStudents).fill(0)
       };
     case PACER_ITEM_TAPPED:
@@ -28,6 +33,18 @@ export default function bmiState(state = {}, action) {
           state.pacerArray[action.index] - 1,
           ...state.pacerArray.slice(action.index + 1)
         ]
+      };
+    case TIME_INTERVAL_ELAPSED:
+      return {
+        ...state,
+        currentShuttle: state.currentShuttle + 1,
+        totalShuttles: state.totalShuttles + 1
+      };
+    case MAX_SHUTTLES_REACHED:
+      return {
+        ...state,
+        currentLevel: state.currentLevel + 1,
+        currentShuttle: 1
       };
     default:
       return state;
