00aa69ba0301f2760462a04194941da66a410cce
Rosaline <rosalinesu@gmail.com>
2017-02-28 12:05:58 -0500

    Modified test structure to pass in the data for testing purposes

--- __tests__/reducer-tests.js
+++ __tests__/reducer-tests.js
@@ -1,4 +1,4 @@
-import reducer from '../src/reducers/student';
+import studentState from '../src/reducers/student';
 import * as MockStudents from '../config/mockStudents';
 import * as studentsAction from '../src/actions/student';
 
@@ -17,36 +17,45 @@ describe('students reducer', () => {
     };
 
     //pass in the action
-    state = reducer(state, studentsAction);
+    state = studentState(state, studentsAction);
 
   it('should return the initial state', () => {
-      let reducer1 = reducer(undefined, {});
+      let reducer1 = studentState(undefined, {});
       expect(reducer1).toEqual(state);
   });
 
   it('should handle STUDENT_FETCH_REQUESTED', () => {
       var fetchStudentsResult = studentsAction.fetchStudents(undefined);
-      state = reducer(state, fetchStudentsResult);
+      state = studentState(state, fetchStudentsResult);
       expect(state.isFetching).toEqual(true);
   });
 
   it('should handle STUDENT_FETCH_SUCCEEDED', () => {
 
-      const first = 'Bob';
-      const last = 'Bobber';
+      const first_name = 'Bob';
+      const last_name = 'Bobber';
       const dob = '09-12-1333';
-      const student = {first, last, dob};
+      const student = {first_name, last_name, dob};
+      // make var students like below
+      state.students = [{first_name, last_name, dob}, {first_name, last_name, dob}];
 
-      var fetchStudentsSuccessAction = studentsAction.fetchStudentsSuccess({first, last, dob});
-      state = reducer(state, fetchStudentsSuccessAction);
-      expect(state.students).toEqual(true);
-      expect(fetchStudentsSuccessAction['students']).toEqual(student);
+
+
+      var fetchStudentsSuccessAction = studentsAction.fetchStudentsSuccess(state.students);
+      console.log(fetchStudentsSuccessAction);
+
+      state = studentState(state, fetchStudentsSuccessAction);
+      expect(state.students).toEqual([student]);
   });
 
-  //new tests added for the 'undo' feature and sorted student list
-  it('should handle ADD_EXISTING_STUDENT_SUCCEEDED'), () => {
-
-  }
+  //new tests added for the sorted student list
+  it('should handle ADD_EXISTING_STUDENT_SUCCEEDED', () => {
+      var student = {program_id: '000', first_name: 'bob', last_name: 'bobber', dob: '09-12-1993'};
+      var addExistingStudentAction = studentsAction.createStudentSuccess(student);
+      state = studentState(state, addExistingStudentAction);
+      console.log(state.students);
+      expect(state.students.length).toEqual(2);
+  });
 /*
 
 
