43065cf78cafc4007392aab277eebd25a21250e5
Rosaline <rosalinesu@gmail.com>
2017-04-13 18:30:28 -0400

    added tests to achieve better coverage

--- __tests__/reducer-bmi-tests.js
+++ __tests__/reducer-bmi-tests.js
@@ -11,7 +11,6 @@ describe('bmi Reducer', () => {
     isFetching: false
   };
 
-
   //pass in the action
   state = bmiState(state, bmiAction);
 
@@ -25,42 +24,10 @@ describe('bmi Reducer', () => {
     state = bmiState(state, saveRequested);
     expect(state.isFetching).toEqual(true);
   });
+
+  it('should handle SAVE_COLLECTED_BMI_DATA_SUCCEEDED', () => {
+    const msg = "data saved";
+    var saveSuccess = bmiAction.saveCollectedBmiDataSuccess(msg);
+    state = bmiState(state, saveSuccess);
+  });
 })
-//  it('returns the same state on an unhandled action', () => {
-//     expect(reducer(undefined, {type: '_NULL'})).toMatchSnapshot();
-// });
-//
-// it('starts BMI collection with a filtered list', () => {
-//     const action = actions.startBMICollection(MockStudents.students);
-//     expect(reducer(undefined, action)).toMatchSnapshot();
-// });
-//
-// it('starts BMI collection', () => {
-//     const action = actions.startBMICollection(MockStudents.studentsWithoutHeightAndWeight);
-//     expect(reducer(undefined, action)).toMatchSnapshot();
-// });
-//
-// it('does not start BMI collection because there are no students created for this program', () => {
-//     const action = actions.startBMICollection([]);
-//     expect(reducer(undefined, action)).toMatchSnapshot();
-// });
-//
-// it('does not start BMI collection because height and weight data has already been collected for all students', () => {
-//     const action = actions.startBMICollection(MockStudents.studentsWithHeightAndWeight);
-//     expect(reducer(undefined, action)).toMatchSnapshot();
-// });
-//
-// it('makes a request to save the data entered for a student', () => {
-//     const action = actions.saveStudent(MockStudents.student1);
-//     expect(reducer(MockStudents.studentsWithoutHeightAndWeight, action)).toMatchSnapshot();
-// });
-//
-// it('saves the student data successfully', () => {
-//     const action = actions.onSaveStudentSuccess(MockStudents.student1);
-//     expect(reducer(MockStudents.studentsWithoutHeightAndWeight, action)).toMatchSnapshot();
-// });
-//
-// it('fails to save the student data', () => {
-//     const action = actions.onSaveStudentFailure(MockStudents.badStudent);
-//     expect(reducer(MockStudents.studentsWithoutHeightAndWeight, action)).toMatchSnapshot();
-// });
