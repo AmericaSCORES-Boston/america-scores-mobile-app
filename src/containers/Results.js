f7b9b1fe94e28f40884df78bc5471917aaf48339
mohigup <gupta.moh@husky.neu.edu>
2017-12-09 19:00:43 -0500

    Temp view for Student Pacer Results

---
+++ src/containers/Results.js
@@ -0,0 +1,136 @@
+import React, { Component, PropTypes } from 'react';
+import { Actions } from 'react-native-router-flux';
+import { View} from 'react-native';
+import { Container, Content, List, ListItem, Footer, FooterTab, Button, H2, H3,Left,Right,Body,Text} from 'native-base';
+import scoresTheme from '../themes/scoresTheme';
+import { connect } from 'react-redux';
+import * as studentActions from '../actions/student';
+import * as eventActions from '../actions/event';
+import dates from '../util/dates';
+
+import styles from '../styles';
+
+class ResultContainer extends Component {
+    constructor(props) {
+        super(props);
+        const student_ids = [], event = null;
+        this.state = {...props.studentsState, ...props.eventsState, student_ids, event};
+        // this.props.component.onRight = () => {
+        //     Actions.addStudent({program_id: this.props.program_id});
+        // };
+        // this.props.component.rightTitle = 'Add';
+    }
+
+    componentWillMount() {
+        this.state.students=this.props.students;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const newStudentsState = nextProps.students;
+        if (newStudentsState && newStudentsState.students) {
+            this.state.students = newStudentsState.students;
+        }
+    }
+
+    // Does this program have any students?
+    hasStudents() {
+        const students = this.state.students || [];
+        return students.length > 0;
+    }
+
+    render() {
+        return (
+            <Container style={styles.container} theme={scoresTheme}>
+                <Content>
+                    <View style={{flexDirection: 'row', flex: 1}}>
+                        {this.showStudents()}
+                        {/*<View style={{flex: 1,height:100,backgroundColor: '#333'}}>*/}
+                            {/*{this.showShuttles()}*/}
+                        {/*</View>*/}
+                    </View>
+
+                </Content>
+                {this.showFooter()}
+            </Container>
+        );
+    }
+
+    // Show the pacer shuttle results
+
+    // showShuttles(){
+    //     const studentArray = this.state.students;
+    //     const students = (
+    //         <List dataArray={studentArray}
+    //               renderRow={(rowData) =>
+    //                   <ListItem>
+    //                       <Text>{rowData.pacer}</Text>
+    //                   </ListItem>
+    //
+    //               }
+    //         />
+    //     );
+    // }
+
+    // Show a list of students for this program if it is not empty.
+    // Otherwise, show a message about the program being empty.
+    showStudents() {
+        const studentArray = this.state.students;
+        const students = (
+            <List dataArray={studentArray}
+                renderRow={(rowData) =>
+                    <ListItem>
+                        <Text>{'Level: ' + rowData.pacer}</Text>
+                        <Text style={{marginLeft:20}}>{'Student: '+rowData.first_name + ' ' + rowData.last_name}</Text>
+                    </ListItem>
+
+                }
+            />
+        );
+
+        const noStudents = (
+            <View>
+                <H3 style={[styles.textAlignCenter, styles.mediumVerticalMargin]}>No data to show.</H3>
+                {/*<H2 style={styles.textAlignCenter}>Click the 'Add' button to get started.</H2>*/}
+            </View>
+        );
+        return (this.hasStudents()) ? students : noStudents;
+    }
+
+
+    // Show a footer with a pacer and bmi collection button if this program has students.
+    // Otherwise, hide the footer.
+    showFooter() {
+        const program = {program_name: this.props.title, program_id: this.props.program_id};
+        const footer = (
+            <Footer>
+                <FooterTab>
+                    {/*<Button active onPress={()=>Actions.pacer({program, students: this.state.students, event: this.state.event})}>*/}
+                        {/*Pacer Test*/}
+                    {/*</Button>*/}
+
+                </FooterTab>
+            </Footer>
+        );
+
+        return (this.hasStudents()) ? footer : null;
+    }
+}
+
+const mapStateToProps = (state) => ({
+    studentsState: state.studentsState,
+    // eventsState: state.eventsState
+});
+//
+// const mapDispatchToProps = (dispatch) => ({
+//     fetchStudents: (program_id) => {
+//         dispatch(studentActions.fetchStudents(program_id));
+//     },
+//     fetchEvents: (program_id) => {
+//         dispatch(eventActions.fetchEvents(program_id));
+//     }
+// });
+
+export default connect(
+    mapStateToProps,
+    // mapDispatchToProps
+)(ResultContainer);
